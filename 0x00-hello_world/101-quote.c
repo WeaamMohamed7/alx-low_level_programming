a5dbf00e1cb0d458a0a985ee5254429055cb93ff
WeaamMohamed7 <weaam.ai_0482@ai.kfs.edu.eg.com>
2023-02-16 18:01:55 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,8 @@
 * main - prints a string and standard error
  * Return: 1                                          
 */
-int main(void)                                       {
+int main(void)
+{
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
