c1a8d4256347d9e3d37dc9713d20282ca090329f
WeaamMohamed7 <weaam.ai_0482@ai.kfs.edu.eg.com>
2023-02-21 15:46:43 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - checks for alphabetical letters
  * @c: a charter to be checke on
  * Return: returns 0 or 1 depending on condition
- */
+*/
 int _isalpha(int c)
 {
   return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
