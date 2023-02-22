122557126e5c5023b96e9e64a4af36643c2de9d9
WeaamMohamed7 <weaam.ai_0482@ai.kfs.edu.eg.com>
2023-02-21 17:29:06 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,31 +2,61 @@
 #include "main.h"
 
 
-/**
-* print_to_98 - prints to 98
-* @n: the starting point
-*/
-void print_to_98(int n)
-{
-  if (n <= 98)
-  {
-    while (n <= 98)
-    {
-      if (n == 98)
-        printf("%d\n", n);
-      else
-        printf("%d, ", n);
-      n = n + 1;
-    }
-  } else if (n > 98)
-  {
-    while (n >= 98)
-    {
-      if (n == 98)
-        printf("%d\n", n);
-      else
-        printf("%d, ", n);
-      n = n - 1;
-    }
-  }
-}
+
+        /**
+
+         * print_to_98 - prints all natural numbers from n to 98,
+
+         * followed by a new line
+
+         * @n: print from this number
+
+         */
+
+        void print_to_98(int n)
+
+        {
+
+                int i, j;
+
+
+
+                if (n <= 98)
+
+                {
+
+                        for (i = n; i <= 98; i++)
+
+                        {
+
+                                if (i != 98)
+
+                                        printf("%d, ", i);
+
+                                else if (i == 98)
+
+                                        printf("%d\n", i);
+
+                        }
+
+                } else if (n >= 98)
+
+                {
+
+                        for (j = n; j >= 98; j--)
+
+                        {
+
+                                if (j != 98)
+
+                                        printf("%d, ", j);
+
+                                else if (j == 98)
+
+                                        printf("%d\n", j);
+
+                        }
+
+                }
+
+        }
