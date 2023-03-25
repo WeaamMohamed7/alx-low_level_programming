103dd2bb7af701d36aec3b3ed7980b3070bada70
weaam mohamed <123474990+WeaamMohamed7@users.noreply.github.com>
2023-03-25 03:05:25 +0200

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
-	#include <stdlib.h>
-	#include <stdio.h>
-	#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
 	/**
 	 * main - Prints the result of simple operations.
 	 * @argc: The number of arguments supplied to the program.
