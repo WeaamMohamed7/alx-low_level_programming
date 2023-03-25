59da5c7760af909263c1564bf0c024276981b1bf
weaam mohamed <123474990+WeaamMohamed7@users.noreply.github.com>
2023-03-25 03:01:33 +0200

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,35 +1,21 @@
 #include <stddef.h>
 #include "function_pointers.h"
-
 /**
- * int_index - a function that searches for an integer
- *
- * @array: pointer to array
- * @size: size of @array
- * @cmp: pointer to function call to check index in array
- *       if it matches
- *
- * Return: Always 0 (Success)
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
 */
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int index;
-	bool y;
-
-	if (array != NULL && size > 0 && cmp != NULL)
-	{
-		if (size <= 0)
-			return (-1);
-
-		for (index = 0; index < size; index++)
-		{
-			y = cmp(array[index]);
-			if (y == TRUE)
-				return (index);
-		}
-	}
-
-	return (-1);
-
+int i;
+if (array == NULL || size <= 0 || cmp == NULL)
+return (-1);
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+return (i);
+}
+return (-1);
 }
