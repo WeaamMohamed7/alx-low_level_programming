b6274f4ce26ae0d2d6f143adaa8305afcc21f84d
weaam mohamed <123474990+WeaamMohamed7@users.noreply.github.com>
2023-03-25 03:52:41 +0200

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,31 +1,36 @@
-#include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+
 /**
- * print_strings - Prints strings, followed by a new line.
- * @separator: The string to be printed between strings.
- * @n: The number of strings passed to the function.
- * @...: A variable number of strings to be printed.
+ * print_strings - a function that prints strings, followed by a new line
  *
- * Description: If separator is NULL, it is not printed.
- *              If one of the strings if NULL, (nil) is printed instead.
+ * @separator: pointer to a constant separator
+ * @n: start of input variables
+ *
+ * Return: nothing
 */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_list strings;
-char *str;
-unsigned int index;
-va_start(strings, n);
-for (index = 0; index < n; index++)
-{
-str = va_arg(strings, char *);
-if (str == NULL)
-printf("(nil)");
-else
-printf("%s", str);
-if (index != (n - 1) && separator != NULL)
-printf("%s", separator);
-}
-printf("\n");
-va_end(strings);
+	va_list ap;
+	unsigned int index;
+	char *str;
+
+	/* iterate the argument list from the start */
+	va_start(ap, n);
+
+	for (index = 0; index < n; index++)
+	{
+		str = va_arg(ap, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+
+		if (separator && index != n - 1)
+			printf("%s", separator);
+	}
+	/*clean up*/
+	va_end(ap);
+	printf("\n");
 }
